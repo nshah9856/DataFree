c8981bfb35d926edf5deaec9ae4c200f5ecb0e21
Nisarg Shah <npshah9856@gmail.com>
2019-01-11 22:45:23 -0800

    added a server

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require("http")
+const express = require("express")
+var bodyParser = require('body-parser');
+const MessagingResponse = require("twilio").twiml.MessagingResponse
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.post('/sms', (req, res) => {
+
+    let user_message = req.body.Body.toLowerCase()
+    const twiml = new MessagingResponse();
+    if (user_message == 'hello') {
+      twiml.message('Hi!');
+    } else if (user_message == 'bye') {
+      twiml.message('Goodbye');
+    } else {
+      twiml.message(
+        'No Body param match, Twilio sends this in the request to your server.'
+      );
+    }
+  
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
+  });
+
+http.createServer(app).listen(1337, ()=> console.log("Listening on port 1337"))
