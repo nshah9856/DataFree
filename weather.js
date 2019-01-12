365dc2e5fa9992887a64337014a5ba97c110bbf0
Nisarg Shah <npshah9856@gmail.com>
2019-01-12 02:34:49 -0800

    hide the auth

--- weather.js
+++ weather.js
@@ -1,10 +1,13 @@
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
+const dotnev = require("dotenv")
+dotenv.load()
+
 module.exports = exports = function getWeather(location, to){
     var twilio = require('twilio');
 
-    var client = new twilio("AC80219c6285881f3fa7c369a4f0cd516f","4657ae8ca93cf3c8984f4838c9a2f62f"); // TODO
+    var client = new twilio(process.env.TWILIO_SID,process.env.TWILIO_AUTH); // TODO
 
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=bfcdb6bb34f68ccc033fb9e297c8df1f`
 
@@ -15,7 +18,7 @@ module.exports = exports = function getWeather(location, to){
         var d = 'Current temperature is: ' + Math.round(out.main.temp) + "\nMinumum today: " + Math.round(out.main.temp_min) + "\nMaximum today: " + Math.round(out.main.temp_max)
         await client.messages.create({
             to: to,
-            from: '+14088821788', // TODO
+            from: '+14088821788', 
             body: `${d}`
         });
         console.log('Request sent');
